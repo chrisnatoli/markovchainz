2645de364aa742920562c58ff2faaa8313157de1
Chris Natoli <chrisnatoli@gmail.com>
2013-11-13 14:05:38 -0600

    fixed mistake with markov process

--- markovchainz.py
+++ markovchainz.py
@@ -6,13 +6,24 @@ from math import exp
 song_filename = 'lupe_fiasco'
 
 
-
+########## Functions
+# Standard matrix-on-vector multiplication.
 def multiply(matrix, vector):
     new_vector = []
     for r in range(len(vector)):
         i = sum([ matrix[r][i]*vector[i] for i in range(len(matrix[r])) ])
         new_vector.append(i)
     return(new_vector)
+
+# Divides each column element by the sum of the column so that
+# column sums to 1 (since it's a probability).
+def normalize_column(matrix, c):
+    sum_column = sum([ matrix[r][c] for r in range(len(matrix)) ])
+    if sum_column == 0:
+        matrix[unique_words.index('\n')][c] = 1
+        sum_column = 1
+    for r in range(len(matrix)):
+        matrix[r][c] = matrix[r][c] / sum_column
 
 
 ########## Read in the input song lyrics.
@@ -44,38 +55,33 @@ for i in range(len(all_words)-1):
     c = unique_words.index(all_words[i])
     r = unique_words.index(all_words[i+1])
     matrix[r][c] = matrix[r][c] + 1
-# Divides each column element by the sum of the column so that
-# column sums to 1 (since it's a probability).
-for c in range(len(unique_words)):
-    sum_column = sum([ matrix[r][c] for r in range(len(unique_words)) ])
-    if sum_column == 0:
-        matrix[unique_words.index('\n')][c] = 1
-        sum_column = 1
-    for r in range(len(unique_words)):
-        matrix[r][c] = matrix[r][c] / sum_column
+for c in range(len(matrix)):
+    normalize_column(matrix, c)
+        
 
 # NOTE: Use 1-e^(-x) somehow where x = current line len - avg line len
 ########## Generate rap.
-# Define initial probability vector.
-probability_vector = [1]
-probability_vector.extend([0] * (len(unique_words)-1))
-num_words = 100
-rap = ''
+# Define initial word by its index in unique_words and set word length of rap.
+word_index = 0
+len_rap = 100
+rap = unique_words[word_index]
 len_line = 0
-for i in range(num_words):
+for i in range(len_rap):
     len_line = len_line + 1
-    # Pick word given currect probability vector.
+    # Pick next word given probability vector for the curent word in the matrix.
+    probability_vector = [ matrix[r][word_index] for r in range(len(matrix)) ]
     rand = random.random()
     sum_so_far = 0
     for j in range(len(probability_vector)):
         sum_so_far = sum_so_far + probability_vector[j]
         if rand < sum_so_far:
-            rap = rap + unique_words[j] + ' '
-            if unique_words[j] == '\n':
+            word_index = j
+            rap = rap + unique_words[word_index]
+            if unique_words[word_index] == '\n':
                 len_line = 0
+            else:
+                rap = rap + ' '
             break
-    # Update probability vector.
-    probability_vector = multiply(matrix, probability_vector)
 
 
 print(rap)
